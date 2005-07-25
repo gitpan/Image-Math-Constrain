1020261071561800ae5a816bcfee0f3fffa78fc6
Adam Kennedy <adamk@cpan.org>
2005-07-25 08:12:19 +0000

    Import of ADAMK/Image-Math-Constrain-1.00 from CPAN.
    gitpan-cpan-distribution: Image-Math-Constrain
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         ADAMK/Image-Math-Constrain-1.00.tar.gz
    gitpan-cpan-author:       ADAMK
    gitpan-cpan-maturity:     released

--- lib/Image/Math/Constrain.pm
+++ lib/Image/Math/Constrain.pm
@@ -5,17 +5,19 @@ package Image::Math::Constrain;
 =head1 NAME
 
 Image::Math::Constrain - Scaling math used in image size constraining (such
-as for thumbnails)
+as thumbnails)
 
 =head1 SYNOPSIS
 
+  use Image::Math::Constrain;
+  
   # Create the math object
   my $Math = Image::Math::Constrain->new(64, 48);
   
   # Get the scaling values for an arbitrary image
   my $Image = My::Image->load("myimage.jpg");
   my $scaling = $Math->constrain($Image->width, $Image->height);
-  die "Don't need to scale" if $scaling->{scale} = 1;
+  die "Don't need to scale" if $scaling->{scale} == 1;
   
   # Returns the three values as a list when called in array contect
   my ($width, $height, $scale) = $Math->constrain(800, 600);
@@ -56,10 +58,10 @@ the image down proportionally so that it fits withint the constraints.
 Of course, they all do it slightly differnetly, and some do it better
 than others.
 
-Image::Math::Constrain has been created to specifically to implement
+C<Image::Math::Constrain> has been created specifically to implement
 this logic once, and implement it properly. Any module or script that
 does image size constraining or thumbnailing should probably be using
-this for it's math.
+this for its math.
 
 =head1 METHODS
 
@@ -71,7 +73,7 @@ use overload 'bool' => sub () { 1 },
 
 use vars qw{$VERSION};
 BEGIN {
-	$VERSION = '0.03';
+	$VERSION = '1.00';
 }
 
 
@@ -100,12 +102,12 @@ to the constraints, and the final width and height of the image after
 scaling, at least one of which should match the constraint.
 
 A value of zero is used to indicate that a dimension should not be
-constrained. Thus, C<<->new(400, 0)>> would indicate to constrain the
+constrained. Thus, C<-E<gt>new(400, 0)> would indicate to constrain the
 width to 400 pixels, but to ignore the height (only changing it to keep
 the image proportional).
 
 The constraint dimensions can be provided in a number of different
-formats. See the Synopsis about for a quick list of these. To stay
+formats. See the Synopsis for a quick list of these. To stay
 compatible with automated constraint generators, you B<can> provide
 constrains as zero width and zero height, and the math object will not
 attempt to do any scaling, always returning the input width/height,
@@ -214,6 +216,9 @@ applies the constrain math to them to get the final width, height
 and the scaling value needed in order to get the your image from
 it's current size to the final size.
 
+The resulting size will be in proportion to the original (it will have
+the same aspect ratio) and will never be larger than the original.
+
 When called in array context, returns the new dimensions and scaling value
 as a list, as in the following.
 
@@ -305,7 +310,7 @@ sub _ret_val {
 
 Bugs should always be submitted via the CPAN bug tracker
 
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Image%3A%3AMath%3A%3AConstrain>
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Image-Math-Constrain>
 
 For other issues, contact the maintainer
 
@@ -318,7 +323,8 @@ the open sourcing and release of this distribution.
 
 =head1 COPYRIGHT
 
-Copyright (c) 2004 Adam Kennedy. All rights reserved.
+Copyright (c) 2004 - 2005 Adam Kennedy. All rights reserved.
+
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
 
